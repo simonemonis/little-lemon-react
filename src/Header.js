5a7d1829b3e3711c73ff8459075fb844774a662f
Simone <you@example.com>
2025-12-24 18:50:42 +0400

    Set up semantic structure, added components, logo, navigation, and meta tags

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import logo from "./assets/logo.png";
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Little Lemon Logo" />
+    </header>
+  );
+}
+
+export default Header;
